d49f6d20dc464f340052f7f9e083b7c6a76c616d
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-08-08 21:32:41 +0200

    updated prime.php

--- prime.php
+++ prime.php
@@ -1,39 +1,33 @@
 <?php 
-	if (count($argv) < 2) {
-		echo "Please provide a number to check if it is a prime.\n";
-	} elseif (is_numeric($argv[1])) {
-		$num = (int) $argv[1];
-		for ($i=1; $i <= $num; $i++) { 
-			if (is_prime($i)) {
-				echo "$i\tPRIME\n";
-			}
-		}
-	} else {
-		echo "'$argv[1]' is not a number.\n";
-	}
+if (count($argv) < 2) {
+    echo "Please provide a number to check if it is a prime.\n";
+} elseif (!is_numeric($argv[1])) {
+    echo "'$argv[1]' is not a number.\n";
+} else {
+    $num = (int) $argv[1];
+    for ($i=2; $i <= $num; $i++) {  // Start from 2 because 1 is not prime
+        if (is_prime($i)) {
+            echo "$i\tPRIME\n";
+        }
+    }
+}
 
-	function is_prime($num) {
-		if ($num < 3) {
-			return true;
-		}
-		for ($i=2; ; $i++) { 
-			if (is_divisor_of($i, $num)) {
-				return false;
-			} elseif ($i * $i >= $num) {
-				return true;
-			}
-		}
-	}
+/**
+ * Check if a number is prime.
+ * 
+ * @param int $num The number to check.
+ * @return bool True if prime, false otherwise.
+ */
+function is_prime($num) {
+    if ($num < 2) {
+        return false;
+    }
 
-	function is_divisor_of($x, $num) {
-		for ($i=1; ; $i++) { 
-			$factor = $x * $i;
-			if ($factor == $num) {
-				echo "$num\t$x * $i\n";
-				return true;
-			} elseif ($factor > $num) {
-				return false;
-			}
-		}
-	}
+    for ($i=2; $i * $i <= $num; $i++) { 
+        if ($num % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 ?>
